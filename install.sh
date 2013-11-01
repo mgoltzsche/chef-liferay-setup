95b73bb2cb02afdf74c6bb5ced4bdbe66d73436c
max <max.goltzsche@gmail.com>
2013-11-01 18:13:29 +0100

    zwischenstand install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if ! test -f "$CHEF_BINARY"; then
 ##############################################################################
 # INSTALLING RVM #############################################################
 ##############################################################################' &&
-    apt-get -y --force-yes install build-essential openssl libreadline6 \
+    apt-get --force-yes -fuy install build-essential openssl libreadline6 \
 	libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev \
 	libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev \
 	libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison &&
