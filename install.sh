55ccf53959c985700a5c344f4334256f5667a7bd
max <max.goltzsche@gmail.com>
2013-11-01 17:19:36 +0100

    zwischenstand install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ if ! test -f "$chef_binary"; then
     apt-get -o Dpkg::Options::="--force-confnew" \
         --force-yes -fuy dist-upgrade &&
     # Install RVM
-    apt-get --force-yes install build-essential openssl libreadline6 \
+    apt-get -y --force-yes install build-essential openssl libreadline6 \
 	libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev \
 	libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev \
 	libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison &&
