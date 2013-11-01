08448a0fcec126816c7376d042fc5b5566a40007
max <max.goltzsche@gmail.com>
2013-11-01 18:06:25 +0100

    zwischenstand install.sh

--- install.sh
+++ install.sh
@@ -13,33 +13,43 @@ if ! test -f "$CHEF_BINARY"; then
 ##############################################################################
 # UPGRADING SYSTEM ###########################################################
 ##############################################################################'
+    &&
     # Upgrade headlessly (this is only safe-ish on vanilla systems)
-    apt-get update &&
+    apt-get update
+    &&
     apt-get -o Dpkg::Options::="--force-confnew" \
-        --force-yes -fuy dist-upgrade &&
+        --force-yes -fuy dist-upgrade
+    &&
     echo '
 ##############################################################################
 # INSTALLING RVM #############################################################
 ##############################################################################'
+    &&
     apt-get -y --force-yes install build-essential openssl libreadline6 \
 	libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev \
 	libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev \
-	libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison &&
-    curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enabled &&
-    echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile &&
-    source ~/.bash_profile &&
+	libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison
+    &&
+    curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enabled
+    &&
+    source /usr/local/rvm/scripts/rvm
+    &&
     echo '
 ##############################################################################
 # INSTALLING RUBY 2.0.0 ######################################################
 ##############################################################################'
+    &&
     rvm install $RUBY_VERSION \
 	--with-openssl-dir=$HOME/.rvm/usr \
-	--verify-downloads 1 &&
-    rvm use --default $RUBY_VERSION &&
+	--verify-downloads 1
+    &&
+    rvm use --default $RUBY_VERSION
+    &&
     echo '
 ##############################################################################
 # INSTALLING CHEF ############################################################
 ##############################################################################'
+    &&
     gem install --no-rdoc --no-ri chef
 fi &&
 
@@ -47,4 +57,5 @@ echo '
 ##############################################################################
 # APPLYING CHEF RECIPES ######################################################
 ##############################################################################'
+&&
 "$CHEF_BINARY" -c solo.rb -j solo.json
