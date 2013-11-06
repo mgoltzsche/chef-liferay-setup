67dcbdf1e0f4ec12b87cdce47bf4f93c050c9cf4
max <max.goltzsche@gmail.com>
2013-11-07 00:23:35 +0100

    removed $

--- cookbooks/algorythm/recipes/liferay.rb
+++ cookbooks/algorythm/recipes/liferay.rb
@@ -46,7 +46,7 @@ execute "Create symlinks and change owner" do
 rm -rf #{node['liferay']['install_directory']}/liferay &&
 ln -s #{liferayHome} #{liferayHomeLink} &&
 ln -s #{liferayHome}/$(ls #{liferayHome} | grep tomcat) #{liferayHome}/tomcat &&
-chown -R #{usr}:#{usr} $#{liferayHome}
+chown -R #{usr}:#{usr} #{liferayHome}
   EOH
   action :nothing
   notifies :run, "execute[Delete *.bat files]", :immediately
