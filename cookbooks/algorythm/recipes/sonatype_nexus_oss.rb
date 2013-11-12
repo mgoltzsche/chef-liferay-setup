230ea00aad9e9aaee34f0f67a6b3d856f29935d6
max <max.goltzsche@gmail.com>
2013-11-12 20:25:40 +0100

    nexus deployed as portlet

--- cookbooks/algorythm/recipes/sonatype_nexus_oss.rb
+++ cookbooks/algorythm/recipes/sonatype_nexus_oss.rb
@@ -2,7 +2,7 @@ usr = node['liferay']['user']
 downloadDir = "/home/#{usr}/Downloads"
 version = node['nexus']['version']
 nexusWarFile = "#{downloadDir}/nexus-#{version}.war"
-nexusDeployDir = "#{node['liferay']['install_directory']}/liferay/tomcat/webapps/nexus"
+nexusDeployWarFile = "#{node['liferay']['install_directory']}/liferay/deploy/nexus.war"
 hostname = node['nexus']['hostname']
 
 # --- Download & deploy Nexus OSS ---
@@ -13,35 +13,11 @@ remote_file nexusWarFile do
   action :create_if_missing
 end
 
-#execute "Deploy Nexus OSS" do
-#  cwd downloadDir
-#  user usr
-#  group usr
-#  command "cp #{nexusWarFile} #{nexusDeployWarFile}"
-#end
-
-directory nexusDeployDir do
-  owner usr
-  group usr
-  mode 00755
-  action :create
-end
-
 execute "Deploy Nexus OSS" do
   cwd downloadDir
   user usr
   group usr
-  command "unzip -qd #{nexusDeployDir} #{nexusWarFile}"
-end
-
-# --- Configure nginx vhost ---
-template "/etc/nginx/sites-available/#{hostname}" do
-  source "nexus.nginx.vhost.erb"
-  mode 00700
-  variables({
-    :hostname => hostname,
-    :liferay_hostname => node['liferay']['hostname']
-  })
+  command "cp #{nexusWarFile} #{nexusDeployWarFile}"
 end
 
 link "/etc/nginx/sites-enabled/#{hostname}" do
