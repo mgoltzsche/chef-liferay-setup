ccdeea29d4fb6645c7fff2f22041960c9aa3ba12
max <max.goltzsche@gmail.com>
2013-11-13 02:19:59 +0100

    openjdk jdk used instead of jre due to missing header files

--- cookbooks/algorythm/recipes/default.rb
+++ cookbooks/algorythm/recipes/default.rb
@@ -1,7 +1,7 @@
 # --- Install prerequisites ---
 package 'imagemagick'
 package 'unzip'
-package 'openjdk-7-jre-headless'
+package 'openjdk-7-jdk'
 
 # --- Set host name ---
 hostname = node['liferay']['hostname']
