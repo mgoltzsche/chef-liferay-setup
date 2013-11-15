bf68a43bed48372461efdaa1a04b3d5835a85121
max <max.goltzsche@gmail.com>
2013-11-15 06:43:18 +0100

    liferay tomcat with connector for fake https

--- cookbooks/algorythm/recipes/liferay.rb
+++ cookbooks/algorythm/recipes/liferay.rb
@@ -163,7 +163,7 @@ template "#{liferayHome}/tomcat/conf/server.xml" do
   source "liferay.tomcat.server.xml.erb"
   mode 00700
   variables({
-    :hostname => hostname,
+    :hostname => node['liferay']['hostname'],
     :http_port => node['liferay']['http_port'],
     :https_port => node['liferay']['https_port']
   })
