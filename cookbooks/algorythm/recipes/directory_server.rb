e145d9c826a9072c6bddc469d58365555dea9047
max <max.goltzsche@gmail.com>
2013-11-20 01:53:09 +0100

    fix

--- cookbooks/algorythm/recipes/directory_server.rb
+++ cookbooks/algorythm/recipes/directory_server.rb
@@ -27,7 +27,7 @@ password = node['ldap']['user_password']
 chars = ('a'..'z').to_a + ('0'..'9').to_a
 salt = Array.new(10, '').collect { chars[rand(chars.size)] }.join('')
 salt = hSalt
-password = '{ssha}' + Base64.encode64(Digest::SHA1.digest(password+salt)+salt).chomp! + "\n"
+password = Base64.encode64(Digest::SHA1.digest(password+salt)+salt).chomp!
 #password = '->   ' + Digest::SHA1.digest(salt+password)+'/'+salt
 
 print password+", length: #{password.length}\n"
