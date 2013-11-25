3a0aeb0c5146a97a62c0a5f49e7f0b83e2e0aac6
max <max.goltzsche@gmail.com>
2013-11-25 23:11:52 +0100

    ldap password_hasher separated

--- cookbooks/algorythm/recipes/password_hasher.rb
+++
@@ -1,6 +0,0 @@
-def sshaPassword(password)
-	chars = ('a'..'z').to_a + ('0'..'9').to_a
-	salt = Array.new(8, '').collect { chars[rand(chars.size)] }.join('')
-	password = '{ssha}' + Base64.encode64(Digest::SHA1.digest(password+salt)+salt).chomp!
-	Base64.encode64(password).chomp!.sub("\n", "\n ")
-end
