1edb260903a30bed8b9a2bfd2f4ee2790d7e5de1
max <max.goltzsche@gmail.com>
2013-12-11 07:15:36 +0100

    backup scripts enhanced: using plain pg sql files and psql to restore (for compatibility reasons), (un)packed backups as tar.bz2 in backup.sh

--- cookbooks/algorythm/recipes/backup.rb
+++ cookbooks/algorythm/recipes/backup.rb
@@ -1,14 +1,34 @@
 executable = "#{node['backup']['install_directory']}/backup.sh"
 
-directory "#{node['backup']['install_directory']}/scripts" do
+directory "#{node['backup']['install_directory']}/tasks" do
   owner 'root'
   group 'root'
   mode 00755
   recursive true
 end
 
+directory "#{node['backup']['install_directory']}/scripts" do
+  owner 'root'
+  group 'root'
+  mode 00755
+end
+
+template "#{node['backup']['install_directory']}/scripts/backup-pg.sh" do
+  source 'backup-pg.sh.erb'
+  owner 'root'
+  group 'root'
+  mode 00755
+end
+
+template "#{node['backup']['install_directory']}/scripts/backup-files.sh" do
+  source 'backup-files.sh.erb'
+  owner 'root'
+  group 'root'
+  mode 00755
+end
+
 template executable do
-  source "backup.sh.erb"
+  source 'backup.sh.erb'
   owner 'root'
   group 'root'
   mode 00755
