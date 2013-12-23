31f75739b9b840d8bf8fa3481912103779995a16
max <max.goltzsche@gmail.com>
2013-12-23 03:39:01 +0100

    install.sh msgs added/changed

--- install.sh
+++ install.sh
@@ -9,40 +9,44 @@ PATH="$PATH:/usr/local/rvm/bin:$GEM_DIR"
 
 # Are we on a vanilla system?
 if [ ! -f "$CHEF_SOLO" ]; then
-    export DEBIAN_FRONTEND=noninteractive
-    echo "\
-##############################################################################
-# UPGRADING SYSTEM ###########################################################
-##############################################################################" &&
-    apt-get update &&
-    apt-get -o Dpkg::Options::="--force-confnew" \
-        --force-yes -fuy dist-upgrade &&
-    echo "\
-##############################################################################
-# INSTALLING RVM #############################################################
-##############################################################################" &&
-    apt-get --force-yes -fuy install build-essential openssl libreadline6 \
-	libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev \
-	libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev \
-	libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison &&
-    \curl -L https://get.rvm.io | bash -s stable --autolibs=enabled &&
-    echo "\
-##############################################################################
-# INSTALLING RUBY ############################################################
-##############################################################################" &&
-    rvm install $RUBY_VERSION \
-	--with-openssl-dir=$HOME/.rvm/usr \
-	--verify-downloads 1 &&
-    rvm use --default $RUBY_VERSION &&
-    echo "\
-##############################################################################
-# INSTALLING CHEF ############################################################
-##############################################################################" &&
-    gem install --no-rdoc --no-ri chef
+	export DEBIAN_FRONTEND=noninteractive
+	echo "\
+==============================================================================
+  UPGRADING SYSTEM
+==============================================================================" &&
+	apt-get update &&
+	apt-get -o Dpkg::Options::="--force-confnew" \
+		--force-yes -fuy dist-upgrade &&
+	echo "\
+==============================================================================
+  INSTALLING RVM
+==============================================================================" &&
+	echo '* Install requirements' &&
+	apt-get --force-yes -fuy install build-essential openssl libreadline6 \
+		libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev \
+		libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev \
+		libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison &&
+	echo '* Compile RVM' &&
+	\curl -L https://get.rvm.io | bash -s stable --autolibs=enabled &&
+	echo "\
+==============================================================================
+  INSTALLING RUBY
+==============================================================================" &&
+	echo "* Install Ruby $RUBY_VERSION" &&
+	rvm install $RUBY_VERSION \
+		--with-openssl-dir=$HOME/.rvm/usr \
+		--verify-downloads 1 &&
+	echo '* Set default Ruby version' &&
+	rvm use --default $RUBY_VERSION &&
+	echo "\
+==============================================================================
+  INSTALLING CHEF
+==============================================================================" &&
+	gem install --no-rdoc --no-ri chef
 fi &&
 
 echo "\
-##############################################################################
-# APPLYING CHEF RECIPES ######################################################
-##############################################################################" &&
+==============================================================================
+  APPLYING CHEF RECIPES
+==============================================================================" &&
 "$CHEF_SOLO" -c solo.rb -j solo.json
