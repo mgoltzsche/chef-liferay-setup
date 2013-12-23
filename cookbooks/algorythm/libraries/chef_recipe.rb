516a5a2c49d6522f79af370d97a7a1c9129b6d68
max <max.goltzsche@gmail.com>
2013-12-23 23:52:11 +0100

    ldap password format

--- cookbooks/algorythm/libraries/chef_recipe.rb
+++ cookbooks/algorythm/libraries/chef_recipe.rb
@@ -7,7 +7,7 @@ class Chef
       chars = ('a'..'z').to_a + ('0'..'9').to_a
       salt = Array.new(8, '').collect { chars[rand(chars.size)] }.join('')
       password = '{ssha512}' + Base64.encode64(Digest::SHA512.digest(password+salt)+salt).chomp!
-      Base64.encode64(password).chomp!.sub("\n", "\n ")
+      Base64.encode64(password).chomp!.gsub!("\n", "\n ")
     end
   end
 end
