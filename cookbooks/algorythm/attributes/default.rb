1a0ac350e3fa75e630693c44bb131555e837293d
max <max.goltzsche@gmail.com>
2013-12-23 22:34:47 +0100

    ldap listening on 127.0.0.1

--- cookbooks/algorythm/attributes/default.rb
+++ cookbooks/algorythm/attributes/default.rb
@@ -9,7 +9,7 @@ default['backup']['install_directory'] = '/opt/backup-scripts'
 default['backup']['backup_directory'] = '/var/backups/managed'
 
 # LDAP configuration
-default['ldap']['listenhost'] = '0.0.0.0'
+default['ldap']['listenhost'] = '127.0.0.1'
 default['ldap']['hostname'] = 'localhost'
 default['ldap']['port'] = 389
 default['ldap']['dirmanager'] = 'dirmanager'
