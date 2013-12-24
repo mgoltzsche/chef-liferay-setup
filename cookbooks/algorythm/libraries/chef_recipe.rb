2389fe8553f73b0e1c3511ba793d3fb5799bbcad
max <max.goltzsche@gmail.com>
2013-12-24 07:22:24 +0100

    decreased ldap password encryption to ssha1 due to liferay compatibility

--- cookbooks/algorythm/libraries/chef_recipe.rb
+++ cookbooks/algorythm/libraries/chef_recipe.rb
@@ -6,7 +6,7 @@ class Chef
     def ldapPassword(password)
       chars = ('a'..'z').to_a + ('0'..'9').to_a
       salt = Array.new(8, '').collect { chars[rand(chars.size)] }.join('')
-      password = '{ssha256}' + Base64.encode64(Digest::SHA256.digest(password+salt)+salt).chomp!
+      password = '{ssha}' + Base64.encode64(Digest::SHA1.digest(password+salt)+salt).chomp!
       Base64.encode64(password).chomp!.gsub!("\n", "\n ")
     end
   end
