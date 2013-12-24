eb306180719eb2d76b1a66aaa06e722d6b58e8da
max <max.goltzsche@gmail.com>
2013-12-24 01:41:06 +0100

    using newly generated certificate

--- cookbooks/algorythm/recipes/certificate.rb
+++ cookbooks/algorythm/recipes/certificate.rb
@@ -1,6 +1,11 @@
 package 'openssl'
 
 execute 'Generate certificate' do
-  command "openssl req -new -newkey rsa:4096 -days 2000 -nodes -x509 -subj '/C=DE/ST=Berlin/L=Berlin/O=algorythm/CN=algorythm.de' -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt"
+  user 'root'
+  group 'root'
+  command <<-EOH
+openssl req -new -newkey rsa:4096 -days 2000 -nodes -x509 -subj "/C=DE/ST=Berlin/L=Berlin/O=algorythm/CN=algorythm.de" -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt &&
+chmod 600 /etc/ssl/private/server.key
+  EOH
   not_if {File.exist?("/etc/ssl/private/server.key")}
 end
