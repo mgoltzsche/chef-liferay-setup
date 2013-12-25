8b945652aa9a9578413830824c7c25756ebab4ae
max <max.goltzsche@gmail.com>
2013-12-26 00:57:59 +0100

    ldapPassword function working with ssha512

--- cookbooks/algorythm/libraries/chef_recipe.rb
+++ cookbooks/algorythm/libraries/chef_recipe.rb
@@ -1,13 +1,13 @@
 class Chef
-  class Recipe
-    def ldapSuffix(domain)
-      domain.split('.').map{|dc| "dc=#{dc}"}.join(',')
-    end
-    def ldapPassword(password)
-      chars = ('a'..'z').to_a + ('0'..'9').to_a
-      salt = Array.new(8, '').collect { chars[rand(chars.size)] }.join('')
-      password = '{ssha}' + Base64.encode64(Digest::SHA1.digest(password+salt)+salt).chomp!
-      Base64.encode64(password).chomp!.gsub!("\n", "\n ")
-    end
-  end
+	class Recipe
+		def ldapSuffix(domain)
+			domain.split('.').map{|dc| "dc=#{dc}"}.join(',')
+		end
+		def ldapPassword(password)
+			chars = ('a'..'z').to_a + ('0'..'9').to_a
+			salt = Array.new(8, '').collect { chars[rand(chars.size)] }.join('')
+			ssha512Password = Base64.encode64(Digest::SHA512.digest(password+salt)+salt).gsub!("\n", '')
+			Base64.encode64("{ssha512}#{ssha512Password}").gsub!("\n", '')
+		end
+	end
 end
