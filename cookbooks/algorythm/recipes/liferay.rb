ec58bf056113dc8f867609a4b0e15fe1d1861c8d
max <max.goltzsche@gmail.com>
2013-12-26 20:56:31 +0100

    default attributes: map initialization

--- cookbooks/algorythm/recipes/liferay.rb
+++ cookbooks/algorythm/recipes/liferay.rb
@@ -141,6 +141,7 @@ end
 # --- Create Liferay postgres user and database
 node['liferay']['shards'].each do |name, shard|
   pgUser = shard['pg']['user']
+print "### PGUSER: #{pgUser} ###\n"
   pgPassword = shard['pg']['password']
   pgDB = shard['pg']['database']
 
