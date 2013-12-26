a0686e287306e16ccb4a9495ca0a441f594e28c0
max <max.goltzsche@gmail.com>
2013-12-26 20:38:14 +0100

    default attributes: map initialization

--- cookbooks/algorythm/attributes/default.rb
+++ cookbooks/algorythm/attributes/default.rb
@@ -11,83 +11,115 @@ default['backup'] = {
 }
 
 # LDAP configuration
-default['ldap']['listenhost'] = '127.0.0.1'
-default['ldap']['hostname'] = 'localhost'
-default['ldap']['port'] = 389
-default['ldap']['dirmanager'] = 'dirmanager'
-default['ldap']['dirmanager_password'] = 'password'
-default['ldap']['domain'] = 'dev.algorythm.de'
-default['ldap']['admin_cn'] = 'admin'
-default['ldap']['admin_sn'] = 'Goltzsche'
-default['ldap']['admin_givenName'] = 'Max'
-default['ldap']['admin_password'] = 'password'
+default['ldap'] = {
+	'listenhost' => '127.0.0.1',
+	'hostname' => 'localhost',
+	'port' => 389,
+	'dirmanager' => 'dirmanager',
+	'dirmanager_password' => 'password',
+	'domain' => 'dev.algorythm.de',
+	'admin_cn' => 'admin',
+	'admin_sn' => 'Goltzsche',
+	'admin_givenName' => 'Max',
+	'admin_password' => 'password'
+}
 
 # Mail Server configuration
-default['mail_server']['hostname'] = 'localhost'
-default['mail_server']['vmail_user'] = 'vmail'
-default['mail_server']['vmail_directory'] = '/var/vmail'
-default['mail_server']['ldap']['user'] = default['mail_server']['vmail_user']
-default['mail_server']['ldap']['password'] = "password"
+default['mail_server'] = {
+	'hostname' => 'localhost',
+	'vmail_user' => 'vmail',
+	'vmail_directory' => '/var/vmail',
+	'ldap' => {
+		'user' => default['mail_server']['vmail_user'],
+		'password' => 'password'
+	}
+}
 
 # Postgresql installation
-default['postgresql']['address'] = "localhost"
-default['postgresql']['port'] = 5432
-default['postgresql']['version'] = "9.1"
+default['postgresql'] = {
+	'address' => "localhost",
+	'port' => 5432,
+	'version' => '9.1'
+}
 
 # Liferay installation
-default['liferay']['download_url'] = "http://downloads.sourceforge.net/project/lportal/Liferay%20Portal/6.2.0%20GA1/liferay-portal-tomcat-6.2.0-ce-ga1-20131101192857659.zip?r=&ts=1383419991&use_mirror=garr"
-default['liferay']['native_connectors_download_url'] = "ftp://ftp.fu-berlin.de/unix/www/apache/tomcat/tomcat-connectors/native/1.1.29/source/tomcat-native-1.1.29-src.tar.gz"
-default['liferay']['apr_download_url'] = "ftp://ftp.fu-berlin.de/unix/www/apache/apr/apr-1.5.0.tar.bz2"
-default['liferay']['install_directory'] = "/opt"
-default['liferay']['home'] = "/var/opt/liferay"
-default['liferay']['user'] = "liferay"
-default['liferay']['catalina_opts'] = "-server -Xms128m -Xmx1024m -XX:MaxPermSize=512m"
-default['liferay']['http_port'] = 8087
-default['liferay']['https_port'] = 8089
+default['liferay'] = {
+	'download_url' => 'http://downloads.sourceforge.net/project/lportal/Liferay%20Portal/6.2.0%20GA1/liferay-portal-tomcat-6.2.0-ce-ga1-20131101192857659.zip?r=&ts=1383419991&use_mirror=garr',
+	'native_connectors_download_url' => 'ftp://ftp.fu-berlin.de/unix/www/apache/tomcat/tomcat-connectors/native/1.1.29/source/tomcat-native-1.1.29-src.tar.gz',
+	'apr_download_url' => 'ftp://ftp.fu-berlin.de/unix/www/apache/apr/apr-1.5.0.tar.bz2',
+	'install_directory' => '/opt',
+	'home' => '/var/opt/liferay',
+	'user' => 'liferay',
+	'catalina_opts' => '-server -Xms128m -Xmx1024m -XX:MaxPermSize=512m',
+	'http_port' => 7080,
+	'https_port' => 7443,
 
-# Liferay Portal configuration
-default['liferay']['defaultshard'] = 'default'
-default['liferay']['shards']['default']['hostname'] = default['ldap']['domain']
-default['liferay']['shards']['default']['pg']['port'] = 5432
-default['liferay']['shards']['default']['pg']['database'] = 'liferay'
-default['liferay']['shards']['default']['pg']['user'] = default['liferay']['user']
-default['liferay']['shards']['default']['pg']['password'] = 'liferay'
-default['liferay']['shards']['dieter-goltzsche']['hostname'] = 'dieter-goltzsche.de'
-default['liferay']['shards']['dieter-goltzsche']['pg']['port'] = 5432
-default['liferay']['shards']['dieter-goltzsche']['pg']['database'] = 'dieter-goltzsche-liferay'
-default['liferay']['shards']['dieter-goltzsche']['pg']['user'] = 'dieter-goltzsche'
-default['liferay']['shards']['dieter-goltzsche']['pg']['password'] = 'password'
-default['liferay']['ldap']['user'] = default['liferay']['user']
-default['liferay']['ldap']['password'] = 'password'
-default['liferay']['system_mail_prefix'] = 'system'
-default['liferay']['company_default_name'] = default['ldap']['domain']
-default['liferay']['admin']['name'] = 'Max Goltzsche'
-default['liferay']['timezone'] = 'Europe/Berlin'
-default['liferay']['country'] = 'DE'
-default['liferay']['language'] = 'de'
-default['liferay']['tomcat_virtual_hosts']['nexus'] = "repository.#{default['ldap']['domain']}"
+	'company_default_name' => default['ldap']['domain'],
+	'system_mail_prefix' => 'system',
+	'admin' => {'name' => 'Max Goltzsche'},
+	'timezone' => 'Europe/Berlin',
+	'country' => 'DE',
+	'language' => 'de',
+	'tomcat_virtual_hosts' => {
+		'nexus' => "repository.#{default['ldap']['domain']}"
+	},
+	'defaultshard' => 'default',
+	'shards' => {
+		'default' => {
+			'hostname' => default['ldap']['domain'],
+			'pg' => {
+				'port' => 5432,
+				'database' => 'liferay',
+				'user' => default['liferay']['user'],
+				'password' => 'liferay'
+			}
+		},
+		'dieter-goltzsche' => {
+			'hostname' => 'dieter-goltzsche.de',
+			'pg' => {
+				'port' => 5432,
+				'database' => 'liferay-dieter-goltzsche',
+				'user' => 'dieter-goltzsche',
+				'password' => 'password'
+			}
+		}
+	},
+	'ldap' => {
+		'user' => 'liferay',
+		'password' => 'password'
+	}
+}
 
 # Sonatype Nexus installation
-default['nexus']['version'] = '2.7.0-06'
-default['nexus']['hostname'] = "nexus.#{default['domainname']}"
-default['nexus']['deploy_directory'] = "#{default['liferay']['install_directory']}/liferay/webapps-nexus"
-default['nexus']['home'] = '/var/opt/nexus'
-default['nexus']['ldap']['user'] = 'nexus'
-default['nexus']['ldap']['password'] = 'password'
-default['nexus']['system_mail_prefix'] = 'system'
+default['nexus'] = {
+	'version' => '2.7.0-06',
+	'hostname' => "repository.#{default['domainname']}",
+	'system_mail_prefix' => 'system',
+	'deploy_directory' => "#{default['liferay']['install_directory']}/liferay/webapps-nexus",
+	'home' => '/var/opt/nexus',
+	'ldap' => {
+		'user' => 'nexus',
+		'password' => 'password'
+	}
+}
 
 # Redmine Backlogs installation
-default['redmine']['install_directory'] = "/opt"
-default['redmine']['home'] = "/var/opt/redmine"
-default['redmine']['user'] = "redmine"
-default['redmine']['version'] = "2.3.3"
-default['redmine']['backlogs_version'] = "v1.0.6"
+default['redmine'] = {
+	'install_directory' => '/opt',
+	'home' => '/var/opt/redmine',
+	'user' => 'redmine',
+	'version' => '2.3.3',
+	'backlogs_version' => 'v1.0.6',
 
-# Redmine Backlogs configuration
-default['redmine']['hostname'] = "redmine.#{default['domainname']}"
-default['redmine']['postgresql']['database'] = "redmine"
-default['redmine']['postgresql']['user'] = default['redmine']['user']
-default['redmine']['postgresql']['password'] = "redmine"
-default['redmine']['ldap']['user'] = default['redmine']['user']
-default['redmine']['ldap']['password'] = "password"
-default['redmine']['system_mail_prefix'] = 'system'
+	'hostname' => "redmine.#{default['domainname']}",
+	'system_mail_prefix' => 'system',
+	'postgresql' => {
+		'database' => 'redmine',
+		'user' => 'redmine',
+		'password' => 'password'
+	},
+	'ldap' => {
+		'user' => default['redmine']['user'],
+		'password' => 'password'
+	}
+}
