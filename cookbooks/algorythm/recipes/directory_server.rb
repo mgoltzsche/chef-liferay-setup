bc598b6d567e9ce9c09f42aaaab55aa66267547c
max <max.goltzsche@gmail.com>
2013-12-27 22:45:48 +0100

    (re)setting dirmanager password SSHA512 encrypted

--- cookbooks/algorythm/recipes/directory_server.rb
+++ cookbooks/algorythm/recipes/directory_server.rb
@@ -34,7 +34,7 @@ ulimit -n #{node['max_open_files']} &&
 setup-ds -sf /tmp/ds-config.inf
 STATUS=$?
 rm -f /tmp/ds-config.inf
-exit $?
+exit $STATUS
 		EOH
 		not_if {File.exist?("/etc/dirsrv/slapd-#{instanceId}")}
 		notifies :run, "execute[Configure #{instanceId} instance]", :immediately
